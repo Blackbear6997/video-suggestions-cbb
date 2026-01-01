e5e9370d2cdc57197f837337d97ac24abab2714e
Asta Sonner <astasonner@Astas-MacBook-Pro.local>
2026-01-02 10:49:04 +1200

    Force unique build ID and stronger cache headers

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  generateBuildId: async () => {
+    return Date.now().toString()
+  },
   headers: async () => [
     {
       source: '/:path*',
       headers: [
         {
           key: 'Cache-Control',
-          value: 'no-store, must-revalidate',
+          value: 'no-cache, no-store, must-revalidate',
+        },
+        {
+          key: 'Pragma',
+          value: 'no-cache',
+        },
+        {
+          key: 'Expires',
+          value: '0',
         },
       ],
     },
